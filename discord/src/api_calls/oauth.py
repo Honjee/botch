27fdbb2c64fc3478d9a6c93886e9d0c440cf37e4
royce <kim.royce2@gmail.com>
2017-09-30 18:54:22 -0400

    testing import

--- discord/src/api_calls/oauth.py
+++ discord/src/api_calls/oauth.py
@@ -1,6 +1,11 @@
 from api_calls.base import baseURL
 from api_calls.base import buildQueryParams
+from api_calls.credentials import client_id
+from api_calls.credentials import client_secret
 
 def oauth(req):
     resp = buildQueryParams(req)
-    print(resp)
+    resp.client_id = client_id
+    resp.client_secret = client_secret
+    print("ouaht fuucnt")
+    return resp
